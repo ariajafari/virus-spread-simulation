4361dc650e7986d4942e592ce698da3fa707b3be
Aria Jafari <ariajafari1994@gmail.com>
2020-03-22 13:05:43 +0430

    making person class

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ list_of_families = l.reshape(5000, 5).tolist()
 np.random.shuffle(l)
 lisit_of_friends = np.reshape(l,(12500,2)).tolist()
 list_of_persons=np.zeros((5000, 5)).tolist()
+
+class Person:
+    def __init__(self,ID,Family_ID,Friend_ID,Health_Status):
+        self.ID=ID 
+        self.Family_ID=Family_ID
+        self.Friend_ID=Friend_ID
+        self.Health_Status=Health_Status
+    def check_health(self):
+        print(self.ID,self.Health_Status)
